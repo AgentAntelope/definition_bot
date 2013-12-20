fb556684d0ef4a9b7095dce658270338f2df371f
Alex Sunderland <alex.sunderland@gmail.com>
2013-12-20 20:57:23 +0000

    Reword regular expression to handle words with spaces in them

--- dictionary.rb
+++ dictionary.rb
@@ -1,14 +1,14 @@
 class Dictionary
 
   DICTIONARY_ARRAY = []
-  
+
   def self.array
   return DICTIONARY_ARRAY if DICTIONARY_ARRAY.size > 1000
     dictionary = File.open('dictionary_file')
     dictionary.each_line do |line|
       h = {}
-      captures = line.match(/(\w+)(.*+)/).captures
-      h[captures.first] = captures.last
+      match = line.match(/(?<word>[\w\- ]+)(?<definition> \w+\..*)/)
+      h[match[:word]] = match[:definition]
       DICTIONARY_ARRAY << h
     end
     DICTIONARY_ARRAY
